89132405769ee02538d4da8e795733b6e8ecf2af
web-proger04 <92223429+web-proger04@users.noreply.github.com>
2022-08-16 13:39:15 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.slider-catalog__slider', {
+   // бесконечность
+   loop: true,
+
+   // навигация (стрелки)
+   navigation: {
+      nextEl: '.slider-catalog__arrow-next',
+      prevEl: '.slider-catalog__arrow-prev',
+   },
+   
+   // скорость
+   speed: 1350,
+
+}); 
+
+
